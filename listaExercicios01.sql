53d55aae1b6069bc1755e0013ba6caad94754d35
Jonatas527 <102703755+Jonatas527@users.noreply.github.com>
2023-09-10 14:12:06 -0300

    Update listaExercicios01.sql
    commit 4

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -19,6 +19,14 @@ FROM livros
 INNER JOIN autores ON livros.autor_id = autores.id
 WHERE autores.nome = 'J.K. Rowling';
 
+4. Encontre todos os alunos matriculados em 'Engenharia de Software':
+
+sql
+SELECT alunos.nome
+FROM alunos
+INNER JOIN matriculas ON alunos.id = matriculas.aluno_id
+WHERE matriculas.curso = 'Engenharia de Software';
+
 
 
 
